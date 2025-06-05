3eae6b46fe84c0423de7246e88b541df393ffcce
MARIA LUIZA AMORIM ROHDE <mariaroh@cptm.info>
2025-06-05 09:28:24 -0300

    Efetuando correções

--- main.py
+++ main.py
@@ -19,22 +19,12 @@ opcao = 0
 # Função responsavel por tratar toda a criação dos usuários
 def criar_usuario(nome, data_nascimento, cpf, endereco):
     # Dicionário responsavel por armazenar as informações de endereço
-    endereco = {logradouro: logradouro,
-                numero_propriedade: numero_propriedade,
-                bairro: bairro,
-                cidade: cidade,
-                sigla_estado: sigla_estado}
     
     # Dicionário responsavel pelas informações do usuário
-    usuario = {nome: nome,
+    usuarios = {nome: nome,
                data_nascimento: data_nascimento,
                cpf: cpf,
                endereco: endereco}
-    
-    nome = input("Informe o nome do usuário: ")
-    data_nascimento = input("Informe a data de nascimento deste do usuário: ")
-    cpf = input("Informe o CPF do usuário: ")
-    endereco = input("Informe o endereço do usuário: ")
      
     # Remove qualquer caracter que não seja letra ou número
     cpf_formatado = ''.join(filter(str.isdigit, cpf))
@@ -46,15 +36,15 @@ def criar_usuario(nome, data_nascimento, cpf, endereco):
 
 # Função responsavel por tratar toda a criação das contas dos usuários
 def criar_conta(agencia, numero_conta, usuario):
-    agencia = 0001
-    numero_conta = 1
+
+    contas = {agencia: agencia,
+              numero_conta: numero_conta,
+              usuario: usuario}
+    
+
     
 # Função responsavel por tratar os saques realizados na conta
-def saque(*, saldo, valor_saque, limite_valor, numero_saque, limite_saque):
-    limite_valor = 500.0
-    limite_saque = 3
-    numero_saque = 0
-    saques_realizados = []
+def saque(*, saldo, valor_saque, limite_valor, extrato, numero_saque, limite_saque):
 
     if numero_saque >= limite_saque: # Caso a quantidade de saques seja maior ou iqual ao limite de saques o mesmo será impedido
                 print("Limite de saques diários atingido.\n")
@@ -62,8 +52,6 @@ def saque(*, saldo, valor_saque, limite_valor, numero_saque, limite_saque):
     # Da continuidade ao saque
     else:
         try:
-            valor_saque = float(input("Digite o quantia a ser retirada: ")) # Recebe o valor a ser retirado da conta
-
             if valor_saque > limite_valor: # Caso o valor enserido seja maior do que o limite é informado e solcitado que insira novamente
                 print(f"O valor máximo para saque é R${limite_valor:.2f}.\n")
 
@@ -76,24 +64,21 @@ def saque(*, saldo, valor_saque, limite_valor, numero_saque, limite_saque):
             else: # Código continua normalmente
                 saldo -= valor_saque
                 numero_saque += 1
-                saques_realizados += [valor_saque]
                 print("Saque realizado!\n")
 
-                return saldo
+                return saldo, extrato
                 
         except ValueError: # Trata erro ao ser inserido um valor string
                 print("Entrada inválida. Por favor, digite um número válido. Não utilize vigulas mas sim ponto. Ex.: 275.49\n")
 
 # Função responsavel por tratar os depósitos realizados na conta
-def deposito(saldo, valor_deposito, /):
+def deposito(saldo, valor_depositado, extrato, /):
 
     try:
-        valor_deposito = float(input("Digite a quantia a ser depositada: ")) # Recebe o valor a ser depositado
-
-        if valor_deposito > 0: # Caso seja um número positivo da continuidade ao código
-            saldo += valor_deposito
+        if valor_depositado > 0: # Caso seja um número positivo da continuidade ao código
+            saldo += valor_depositado
             print("Depósito realizado! \n")
-            return saldo
+            return saldo, extrato
 
         else: # Caso o valor informado seja invalido é informado ao usuário e solicitado novamente a inserção
             print("O valor deve ser positivo.\n")
@@ -115,6 +100,19 @@ def extrato(saldo, /, *, extrato):
 
 # Função responsavel por guiar o usuário pelas funções do sistema
 def menu():
+
+    LIMITE_SAQUE = 3
+    LIMITE_VALOR_SAQUE = 500.0
+
+    saldo = 0.0
+    extrato = []
+    numero_saque = 0
+
+    usuarios = []
+    contas = []
+    numero_conta = 1
+    AGENCIA = "0001"
+
     # Loop que mantem o sistema funcionando até que seja escolhida a opção 4 para sair
     while opcao != 0:
         print("""
@@ -128,14 +126,24 @@ def menu():
             ========================
             """)
         try:
-            opcao = int(input("Digite a opção desejada: ")) # Recebe a opção escolhida pelo usuário
+            opcao = int(input("Digite a opção desejada: ")).strip() # Recebe a opção escolhida pelo usuário
             # Ao ser escolhida a opção 1 é retoranada a função deposito
             if opcao == 1:
                 return deposito
                 
             # Ao ser escolhida a opção 2 é retoranada a função saque
             elif opcao == 2:
-                return saque
+
+                valor = float(input("Informe o valor do saque: "))
+
+                saldo, extrato, numero_saque = saque(
+                saldo=saldo,
+                valor_saque=valor,
+                extrato=extrato,
+                limite_valor=LIMITE_VALOR_SAQUE,
+                numero_saque=numero_saque,
+                limite_saque=LIMITE_SAQUE
+            )
                 
             # Ao ser escolhida a opção 3 é retoranada a função extrato
             elif opcao == 3:
@@ -144,7 +152,21 @@ def menu():
             
             # Ao ser escolhida a opção 4 é retoranada a função criar usuário
             elif opcao == 4:
-                return criar_usuario
+                
+                endereco = {logradouro: logradouro,
+                numero_propriedade: numero_propriedade,
+                bairro: bairro,
+                cidade: cidade,
+                sigla_estado: sigla_estado}
+
+                nome = input("Informe o nome do usuário: ")
+                data_nascimento = input("Informe a data de nascimento do usuário: ")
+                cpf = input("Informe o CPF do usuário: ")
+
+                nome, data_nascimento, cpf = criar_usuario(
+                nome=nome,
+                data_nascimento=data_nascimento,
+                cpf=cpf)
             
             # Ao ser escolhida a opção 5 é retoranada a função criar conta
             elif opcao == 5:
@@ -159,3 +181,6 @@ def menu():
 
         except ValueError: # Trata erro ao ser inserido um valor string
                         print("Entrada inválida. Digite somente números. \n")
+
+if __name__ == "__menu__":
+    menu()
